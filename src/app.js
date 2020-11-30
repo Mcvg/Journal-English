b95cb8b616fa98c8481ffb3bc8e4e46497213841
DESKTOP-U1HOUT7\Maria Camila Vergara <mvergara@liberascf.com>
2020-11-29 19:19:16 -0500

    Connection database

--- src/app.js
+++ src/app.js
@@ -13,6 +13,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/activity', activityRoutes);
 app.use('/user', userRoutes);
 
+// Database configuration
+let dev_db_url = 'mongodb://kath:kath123@ds263917.mlab.com:63917/rummy';
+
+// Connect to mongodb
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // Handle error
 app.use((error, req, res, next) => {
   const status = error.statusCode || 500;
