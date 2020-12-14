65fac7a2b0464e8a8937590bcce0133f2c69e2f5
Kath_code <kathmene27@gmail.com>
2020-12-13 19:31:28 -0500

    Validate email and password to get a token

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,16 +1,20 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 // Model DB
 const User = require('../models/user');
 
 exports.getNewToken = async (req, res, next) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email, password }).exec();
+  const user = await User.findOne({ email }).exec();
 
   if (!user) {
-    res.status(401).send('Authentication failed');
+    res.status(401).send('No user found');
   }
 
-  if (user) {
+  var passwordIsValid = bcrypt.compareSync(password, user.password);
+  if (!passwordIsValid) return res.status(401).send('Authentication filed');
+
+  if (passwordIsValid) {
     const token = jwt.sign({ id: user.id }, process.env['API_KEY'], {
       expiresIn: 86400, // expires in 24 hours
     });
